59db8cbcbb90da25abe36833e16f9cd4a98a3fe0
Bernhard Walter <bwa@norcom.de>
2018-05-28 09:46:44 +0200

    added oozie test

--- test.sh
+++ test.sh
@@ -3,3 +3,6 @@ curl -s localhost:50070/webhdfs/v1/?op=LISTSTATUS | jq .
 
 echo "Testing YARN ..."
 curl -s localhost:8088/ws/v1/cluster/scheduler | jq .
+
+echo "Testing Oozie ..."
+curl -s localhost:11000/oozie/v1/jobs | jq .
